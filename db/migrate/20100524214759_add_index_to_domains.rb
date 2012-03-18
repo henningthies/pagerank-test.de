5425084eecc267981410e6ccd15673f1aed0bc72
Henning Thies <thies.henning@gmail.com>
2012-03-18 22:02:14 +0100

    clear branch

--- db/migrate/20100524214759_add_index_to_domains.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexToDomains < ActiveRecord::Migration
-  def self.up
-    add_index :domains, :url, :unique => true
-  end
-
-  def self.down
-    remove_index :domains, :url
-  end
-end
